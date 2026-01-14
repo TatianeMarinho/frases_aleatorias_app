498fc7927ddb6b6016599f76e900aea571b130e9
Tatiane Marinho <devtatianemarinho@gmail.com>
2026-01-14 17:49:46 -0400

    Funcionalidade de gerar frases

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -19,6 +21,32 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   //classe que define o app
+
+  final _frases = [
+    "Enquanto tu for o que os outros querem, tu não vai ser nada",
+    "Separados somos fortes, juntos, imbatíveis",
+    "Erros do passado, vale repensar",
+    "Quem não é solução também faz parte do problema",
+    "Prefiro cair do que me curvar",
+    "Subversivos não nascem prontos, são moldados",
+    "E se a carapuça serviu, normal, Crianças sempre levam tudo pro lado pessoal",
+    "Abelha não explica pra mosca que mel é melhor que merda",
+    "Ninguém liga pra tua história até você vencer, então, vença",
+    "Preso ao progresso, é a minha sentença",
+    "Enquanto avanço, eu vejo no retrovisor aquilo que foi conquistado",
+    "Todo dia quero, Me libertar aos poucos da melancolia",
+    "O mundo pertence aos que são mais pra frente",
+  ];
+
+  var _fraseGerada = "Clique abaixo para gerar uma frase!";
+
+  void _gerarFrase() {
+    var sorteador = Random().nextInt(_frases.length);
+
+    setState(() {
+      _fraseGerada = _frases[sorteador];
+    });
+  }
 
   @override //?
   Widget build(BuildContext context) {
@@ -47,7 +75,7 @@ class _HomeState extends State<Home> {
             children: <Widget>[
               Image.asset("images/logo.jpg"),
               Text(
-                "Clique abaixo para gerar uma frase!",
+                _fraseGerada,
                 textAlign: TextAlign.justify,
                 style: TextStyle(
                   fontSize: 17,
@@ -57,7 +85,7 @@ class _HomeState extends State<Home> {
               ),
               ElevatedButton(
                 //botao com sombra
-                onPressed: () {}, //funçao anonima
+                onPressed: _gerarFrase, //açao do botao
                 style: ElevatedButton.styleFrom(
                   //onde define o design do botao
                   backgroundColor: Colors.deepOrange, // cor de fundo
